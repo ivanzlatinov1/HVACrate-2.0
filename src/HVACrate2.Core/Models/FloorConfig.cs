bf6854e90b3384d265521fa805b6ea902aad6bec
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2026-08-03 22:44:50 +0300

    Added project files

---
+++ src/HVACrate2.Core/Models/FloorConfig.cs
@@ -0,0 +1,12 @@
+namespace HVACrate2.Core.Models;
+
+public class FloorConfig
+{
+    public string DxfPath { get; set; } = "Brizstroy_Misari.dxf";
+    public string ExcelPath { get; set; } = "ЕЕ_МИСАРИ.xlsx";
+    public int FloorRow { get; set; } = 7;   // A7 = I етаж, в блока "Геометрични характеристики"
+    public int WallRow { get; set; } = 31;   // A31 = I етаж, в блока "Описание на стените"
+    public double FloorHeightM { get; set; } = 2.89;
+    public double NorthDeg { get; set; } = 0.0;
+    public string WallLayer { get; set; } = "_A [walls]";
+}
---
+++ tests/HVACrate2.Core.Tests/Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace HVACrate2.Core.Tests;
+
+public class Tests
+{
+    [Test]
+    public async Task Example()
+    {
+        // TODO
+    }
+}
